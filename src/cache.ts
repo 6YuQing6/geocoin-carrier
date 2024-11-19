2c59291604084bbe0c5c145b3a0686f5d63e01de
Sunny Han <shan66@ucsc.edu>
2024-11-18 19:26:43 -0800

    cleaning cahce

--- src/cache.ts
+++ src/cache.ts
@@ -7,37 +7,15 @@ const MAX_COINS = 5;
 import { Cell } from "./board.ts";
 import luck from "./luck.ts";
 
-export class Cache implements Momento<string> {
+class BaseMomento implements Momento<string> {
   i: number;
   j: number;
-  numCoins: number;
-  constructor(cell: Cell) {
-    this.i = cell.i;
-    this.j = cell.j;
-    this.numCoins = Math.ceil(
-      luck([cell.i, cell.j, "initialValue"].toString()) * MAX_COINS,
-    );
-  }
-  toMomento() {
-    return JSON.stringify({ i: this.i, j: this.j, numCoins: this.numCoins });
-  }
 
-  fromMomento(momento: string) {
-    const { i, j, numCoins } = JSON.parse(momento);
-    this.i = i;
-    this.j = j;
-    this.numCoins = numCoins;
-    return this;
-  }
-}
-
-export class Point implements Momento<string> {
-  i: number;
-  j: number;
   constructor(cell?: Cell) {
     this.i = cell ? cell.i : 0;
     this.j = cell ? cell.j : 0;
   }
+
   toMomento() {
     return JSON.stringify({ i: this.i, j: this.j });
   }
@@ -47,5 +25,34 @@ export class Point implements Momento<string> {
     this.i = i;
     this.j = j;
     return this;
+  }
+}
+
+export class Cache extends BaseMomento {
+  numCoins: number;
+
+  constructor(cell: Cell) {
+    super(cell);
+    this.numCoins = Math.ceil(
+      luck([cell.i, cell.j, "initialValue"].toString()) * MAX_COINS,
+    );
+  }
+
+  override toMomento() {
+    const baseMomento = JSON.parse(super.toMomento());
+    return JSON.stringify({ ...baseMomento, numCoins: this.numCoins });
+  }
+
+  override fromMomento(momento: string) {
+    const { numCoins } = JSON.parse(momento);
+    super.fromMomento(momento);
+    this.numCoins = numCoins;
+    return this;
+  }
+}
+
+export class Point extends BaseMomento {
+  constructor(cell?: Cell) {
+    super(cell);
   }
 }
